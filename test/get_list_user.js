369e3a5942c8371b2b2fdcf38b6b40ef95c96d89
bianmtrf <bian@ideku.id>
2025-03-06 12:53:08 +0700

    fix scenario invalid auth

--- test/get_list_user.js
+++ test/get_list_user.js
@@ -12,7 +12,7 @@ let
 describe(`@GET ${testCase.getListUser.description}`, async () => {
     describe(`@GET ${testCase.getListUser.description} - Positive Case`, async () => {
         it(testCase.getListUser.positive.validData, async () => {
-            const response = await gorest.getListUser(pathGetListUser);
+            const response = await gorest.getListUser(pathGetListUser, process.env.TOKEN);
             assert(response.status).to.equal(200);
             assert(response.body).to.be.an('array').that.is.not.empty;
             response.body.forEach(user => {
@@ -21,10 +21,10 @@ describe(`@GET ${testCase.getListUser.description}`, async () => {
         });
     });
     describe(`@GET ${testCase.getListUser.description} - Negative Case`, async () => {
-        it.only(testCase.getListUser.negative.emptyAuth, async () => {
-            const response = await gorest.getListUser(pathGetListUser).unset('Authorization');
+        it.only(testCase.getListUser.negative.invalidAuth, async () => {
+            const response = await gorest.getListUser(pathGetListUser, 'a');
             assert(response.status).to.equal(401);
-            assert(response.body.message).to.equal('Authentication failed');
+            assert(response.body.message).to.equal('Invalid token');
         });
     });
 });
--- test/gorest.js
+++ test/gorest.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const supertest = require('supertest');
 const api = supertest(process.env.BASE_URL);
 
-const getListUser = (path) => api.get(path)
+const getListUser = (path, token) => api.get(path)
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json')
-    .set('Authorization', `Bearer ${process.env.TOKEN}`)
+    .set('Authorization', `Bearer ${token}`)
 
 module.exports = {
     getListUser
